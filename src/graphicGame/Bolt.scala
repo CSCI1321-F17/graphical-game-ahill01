dae8418a26a2a3104723ea81a7b3be474da38ec4
Allison Hill <ahill@trinity.edu>
2017-12-06 16:18:18 -0600

    Intersect is not working for some reason

--- src/graphicGame/Bolt.scala
+++ src/graphicGame/Bolt.scala
@@ -5,24 +5,30 @@ package graphicGame
 
 class Bolt(private var _x: Double, private var _y: Double, level: Level) extends Entity {
  level += this
-  
+ var hit = false 
  var counter = 0.0
   def cx: Double = _x
   def cy: Double = _y
   def width: Double = 2
   def height: Double = 2
   def update(dt: Double): Unit = {
-    _y -= dt
-    _x -= dt
-    counter += dt
+   _y -= dt
+   _x -= dt
+  counter += dt
   }
  override def move(dx: Double, dy: Double): Unit = {
    if (level.maze.isClear(cx+dx, cy+dy, width, height)) {
       _x += dx
       _y += dy
-    }
+    } else alive == false
    }
   
+ def hitSomething():Boolean = {
+   for (i <- 0 until this.level.entities.length) {
+    if(this.intersect(this.level.entities(i))) hit |= true
+  }
+ hit
+ }
  
   def intersect(other: Entity): Boolean = {
     val intersectX = (cx - other.cx).abs < (width + other.width) / 2
@@ -31,8 +37,10 @@ class Bolt(private var _x: Double, private var _y: Double, level: Level) extends
   }
   
 def getStyle:Int = 4
-
-def alive:Boolean = counter < 1.0
+/*
+ * Bolts dissappear when they hit another entity 
+ */
+def alive:Boolean = if((counter < 3.0)|| this.hitSomething == false) true else false
 
 }
 
